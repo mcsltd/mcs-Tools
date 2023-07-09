882ecfb3c21ee297212eab9d7e77c42d80af3bc7
Andrey Morozov <and.morozov.v@gmail.com>
2023-07-10 01:02:28 +0300

    add new version files

--- app.py
+++ app.py
@@ -1,83 +1,72 @@
+import datetime
+
 from tkinter import *
+from tkinter import filedialog as fd
 
 
 class App:
-    def __init__(self, master, func):
-        """
-        Constructor
-        :param master:
-        :param func:
-        """
-        self.master = master
 
-        # function processing csv file
+    def __init__(self, master, func):
+        self.master = master
         self.func = func
 
-        # text file names for saving, processing
-        self.input_file = None
-        self.template_file = "template.txt"
-        self.save_location = None
+        self.processed_files = ""
+        self.template_file = ""
 
-        # input field for the name of processed files
-        self.lab_input = Label(master, text="Введите файл или папку для обработки: ")
-        self.ent_input = Entry(master, width=20)
-
-        # substitution template file
-        self.lab_template = Label(master, text="Введите название файла-шаблона: ")
-        self.ent_template = Entry(master, width=20)
-
-        # where to save processed files
-        self.lab_save = Label(master, text="Введите место сохранения обработанного файлов: ")
-        self.ent_save = Entry(master, width=20)
-
-        # button start process
-        self.btn_start = Button(master, text="Cтарт")
+        # buttons for action
+        self.btn_processed = Button(self.master, text="Выберите файл(-ы) для обработки")
+        self.btn_template = Button(self.master, text="Выберите шаблон")
+        self.btn_start = Button(self.master, text="Cтарт")
 
         # button click event binding
-        self.btn_start["command"] = self.press_start
+        self.btn_processed["command"] = self._get_processed_files
+        self.btn_template["command"] = self._get_template
+        self.btn_start["command"] = self._press_start
 
-        self._setUI()
+        self.info = Text()
+        self.scroll = Scrollbar(command=self.info.yview)
+        self.info.config(yscrollcommand=self.scroll.set)
 
-    def _setUI(self):
-        """
-        Сustomization and arrangement of elements in the application
-        :return:
-        """
+        self.setUI()
+
+    def setUI(self):
         # name of application
         self.master.title("Обработчик .csv файлов")
 
-        # get width and height of main screen
-        width = self.master.winfo_screenwidth()
-        height = self.master.winfo_screenheight()
-        width_app = 575
-        heght_app = 180
+        # place button
+        self.btn_processed.grid(row=2, column=0, sticky=W, padx=10, pady=10)
+        self.btn_template.grid(row=3, column=0, sticky=W, padx=10, pady=10)
+        self.btn_start.grid(row=4, column=0, sticky=W, padx=10, pady=10)
 
-        # set application location
-        loc = f'{width_app}x{heght_app}+{height // 2 + heght_app // 2}+{width // 2 + width_app // 2}'
-        self.master.geometry(loc)
+        # place info
+        self.info.grid(row=0, column=2, rowspan=6, columnspan=2, pady=10, sticky=E)
 
-        self.lab_input.grid(row=0, column=0, sticky=W, padx=10, pady=10)
-        self.ent_input.grid(row=0, column=1, sticky=E, padx=10, pady=10)
-
-        self.lab_template.grid(row=1, column=0, sticky=W, padx=10, pady=10)
-        self.ent_template.grid(row=1, column=1, sticky=E, padx=10, pady=10)
-
-        self.lab_save.grid(row=2, column=0, sticky=W, padx=10, pady=10)
-        self.ent_save.grid(row=2, column=1, sticky=E, padx=10, pady=10)
-
-        self.btn_start.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
-
-    def press_start(self):
-        """
-        Button click handler
-        :return:
-        """
-        # getting data from a field
-        self.input_file = self.ent_input.get()
-        self.template_file = self.ent_template.get()
-        self.save_location = self.ent_save.get()
-
-        self.func(self.input_file, self.template_file, self.save_location)
+        # scroll bar
+        self.scroll.grid(row=0, rowspan=6, column=4, pady=1, sticky="wns")
 
 
+    def _get_processed_files(self):
+        self.processed_files = fd.askopenfilenames(
+            title="Выберите файл(-ы) для обработки",
+            filetypes=(("CSV Files", "*.csv"),)
+        )
 
+    def _get_template(self):
+        self.template_file = fd.askopenfile(
+            title="Выберите файл-шаблон подстановки",
+            filetypes=(("TXT Files", "*.txt"),)
+        )
+
+    def _press_start(self):
+        name_save_dir = f"output-{str(datetime.datetime.now())}"
+        if self.template_file == "":
+            self.info.insert(END, "Не выбран шаблон .txt!\n")
+        elif self.processed_files == "":
+            self.info.insert(END, "Не выбраны .csv файлы для обработки!\n")
+        else:
+            self.info.insert(END, "Начало обработки файлов...\n")
+            for fn in self.processed_files:
+                self.info.insert(END, f"Обрабатывается файл {fn}\n")
+                # print(self.template_file.name)
+                self.func(fn, self.template_file.name, name_save_dir)
+                self.info.insert(END, f"Обработка файла {fn} завершена\n")
--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import csv
-import os
+import datetime
+import os.path
 
-SYMBOLS = ["\'", "\"", "\n"]
+from new_app import *
 
 TEMPLATE_NAME_1 = "Designator"
-TEMPLATE_NAME_2 = "~FV"
-
 NAME_BUFFER_FILE = "temp.csv"
-
+TEMPLATE_NAME_2 = "~FV"
+SYMBOLS = ["\'", "\"", "\n"]
 
 
 def preprocess_data(name_data):
-
     with open(name_data, "r") as txt_file, open(NAME_BUFFER_FILE, "w") as temp:
         flag = False
+        i = 0
+        j = 0
         for line in txt_file.readlines():
 
             # looking for the beginning of the cap
             if TEMPLATE_NAME_1 in line:
                 flag = True
+                # if i != 0:
+                #     # app.info.insert(END, "  Шапка до \"Designator\" удалена!\n")
+                # else:
+                #     # app.info.insert(END, "  В файле не была найдена шапка!\n")
 
             if flag:
                 if not(TEMPLATE_NAME_2 in line):
                     print(line.replace("\"", ""), file=temp, end="")
+                else:
+                    j += 1
+            if j > 0:
+                pass
+                # app.info.insert(END, f" {j} строк содержащих {TEMPLATE_NAME_2} было удалено\n")
+            i += 1
 
 
 def get_templates(template):
@@ -37,63 +48,103 @@ def get_templates(template):
     return t
 
 
-def process_data(data, template):
+def create_dir(name_dir):
+    if os.path.exists(name_dir):
+        return
+    else:
+        os.mkdir(name_dir)
+        # app.info.insert(END,
+        #                 f"Обработанные .csv файлы сохраняются в папке {name_dir}\n"
+        #                 )
+
+
+def process_csvfile(csvfile, template, name_save_dir):
     # remove unwanted lines and symbols
-    preprocess_data(data)
+    preprocess_data(csvfile)
 
     # get templates
     templates = get_templates(template)
 
-    file_output = "output_" + data
+    name_top_file = f"{name_save_dir}/TOP_{csvfile}"
+    name_bot_file = f"{name_save_dir}/BOT_{csvfile}"
+    name_del_file = f"{name_save_dir}/DELETE_{csvfile}"
 
-    with open(NAME_BUFFER_FILE) as csv_file, open(file_output, "w", newline="") as out_csv_file:
+    # app.info.insert(END, f"Созданы три файла для вывода обработанных значений:\n"
+    #                      f" 1){name_top_file};\n"
+    #                      f" 2){name_bot_file};\n"
+    #                      f" 3){name_del_file}.")
 
-        csv_reader = list(csv.DictReader(csv_file))
+
+    with open(NAME_BUFFER_FILE) as r_file,\
+            open(name_top_file, "w", newline="") as top_file,\
+            open(name_bot_file, "w", newline="") as bot_file,\
+            open(name_del_file, "w", newline="") as del_file:
+
+        csv_reader = list(csv.DictReader(r_file))
         name_keys = list(csv_reader[0].keys())
 
-        csv_writer = csv.DictWriter(out_csv_file, fieldnames=name_keys.copy())
-        csv_writer.writeheader()
+        csv_writer_top = csv.DictWriter(top_file, fieldnames=name_keys.copy())
+        csv_writer_top.writeheader()
 
-        proc_data = []
+        csv_writer_bot = csv.DictWriter(bot_file, fieldnames=name_keys.copy())
+        csv_writer_bot.writeheader()
+
+        csv_writer_del = csv.DictWriter(del_file, fieldnames=name_keys.copy())
+        csv_writer_del.writeheader()
+
+        proc_data_top = []
+        proc_data_bot = []
+        proc_data_del = []
+
         name_keys.append(None)
+
+        flag_top = False
+        flag_bot = False
+        flag_del = False
 
         for row in csv_reader:
             for sign in name_keys:
+
+                if sign == "Footprint":
+                    if row[sign] in templates:
+                        row[sign] = templates[row[sign]]
+                    else:
+                        flag_del = True
 
                 if sign == "Rotation":
                     row[sign] = templates[row[sign]]
 
                 if sign == "Layer":
+                    if row[sign] == "TopLayer" and not flag_del:
+                        flag_top = True
+                    elif row[sign] == "BotLayer" and not flag_del:
+                        flag_bot = True
                     row[sign] = templates[row[sign]]
-
-                if sign == "Footprint":
-                    if row[sign] in templates:
-                        if templates[row[sign]] == "delete":
-                            row[sign] = ""
-                        else:
-                            row[sign] = templates[row[sign]]
 
                 if sign == "Comment":
                     row[sign] = row[sign].replace(" ", "_").replace("\n", "")
 
                 if sign in row and sign == None:
-                    row[name_keys[len(name_keys) - 2]] = row[name_keys[len(name_keys) - 2]] + row[sign][0].replace("\n", "")
+                    row[name_keys[len(name_keys) - 2]] = row[name_keys[len(name_keys) - 2]] + row[sign][0].replace("\n",
+                                                                                                                   "")
                     row.pop(sign)
-            proc_data.append(row.copy())
 
-        csv_writer.writerows(proc_data)
+            if flag_bot:
+                proc_data_bot.append(row)
+            elif flag_top:
+                proc_data_top.append(row)
+            elif flag_del:
+                proc_data_del.append(row.copy())
 
+        top_file.writerows(proc_data_top)
+        bot_file.writerows(proc_data_bot)
+        del_file.writerows(proc_data_top)
 
 
 def main():
-    inpt = input("Input data: ")
-    template = input("Input name template: ")
-    process_data(inpt, template)
-
-    try:
-        os.remove(NAME_BUFFER_FILE)
-    except Exception as err:
-        print(f"Cannot remove file {err}")
+    root = Tk()
+    app = App(root, process_csvfile)
+    root.mainloop()
 
 
 if __name__ == "__main__":
