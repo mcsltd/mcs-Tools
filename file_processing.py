bbe8db965aea11af46394be8fb6211cda14b9e43
Andrey Morozov <and.morozov.v@gmail.com>
2023-11-29 16:48:41 +0300

    add class for processing csv file

---
+++ file_processing.py
@@ -0,0 +1,13 @@
+class CsvFile:
+    def __init__(self, name_csv_file):
+        self.name_csv_file = name_csv_file
+        pass
+
+    def preprocessing(self):
+        pass
+
+    def excel_file_processing(self):
+        pass
+
+    def txt_file_processing(self):
+        pass
