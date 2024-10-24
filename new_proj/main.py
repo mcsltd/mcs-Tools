42751adc0e143535fb56dc3a2444095002cf080e
Andrey Morozov <and.morozov.v@gmail.com>
2024-10-24 22:29:46 +0300

    add draw counter in dxf file

--- new_proj/main.py
+++ new_proj/main.py
@@ -1,4 +1,5 @@
 import ezdxf
+from ezdxf import select
 
 from reportlab.lib.pagesizes import A3
 from reportlab.lib.units import mm, cm
@@ -23,13 +24,22 @@ X_PAD = 14 * mm
 Y_PAD = 3 * mm
 
 
-def main(max_number_sticker, path_to_stick, path_to_cut_counter, path_to_save=None):
-    # dxf
-    doc = ezdxf.new()
+def main(max_number_sticker, path_to_stick, path_to_counter, path_to_save_pdf=None):
+    # read counter dxf
+    doc = ezdxf.readfile(path_to_counter)
     msp = doc.modelspace()
 
+    # create new dxf
+    new_doc = ezdxf.new("R2007")
+    new_msp = new_doc.modelspace()
+
+    window = select.Window(
+        (float("-inf"), float("-inf")),
+        (float("inf"), float("inf")), )
+    entities = select.bbox_outside(window, msp.entity_space.entities)
+
     # pdf
-    c = Canvas(path_to_save, pagesize=A3)
+    c = Canvas(path_to_save_pdf, pagesize=A3)
 
     x, y = X_PAD, Y_PAD
     number_sticker = 0
@@ -60,6 +70,11 @@ def main(max_number_sticker, path_to_stick, path_to_cut_counter, path_to_save=No
             plot=c, x=x, y=y, lot="123443"
         )
 
+        for entity in entities:
+            cp_entity = entity.copy()
+            cp_entity.translate(x / mm, y / mm, 0)
+            new_msp.add_foreign_entity(cp_entity)
+
         x += X_DISTANCE + WIDTH_STICKER
         number_sticker += 1
 
@@ -70,9 +85,9 @@ def main(max_number_sticker, path_to_stick, path_to_cut_counter, path_to_save=No
         radius=DIAMETER_POINT / 2
     )
     # down left circle in dxf
-    msp.add_circle(
-        (X_DISTANCE + DIAMETER_POINT / 2, 2*Y_PAD + DIAMETER_POINT / 2),
-        DIAMETER_POINT / 2
+    new_msp.add_circle(
+        ((X_DISTANCE + DIAMETER_POINT / 2) / mm, (2*Y_PAD + DIAMETER_POINT / 2) / mm),
+        DIAMETER_POINT / 2 / mm
     )
 
     # down right circle in pdf
@@ -83,37 +98,49 @@ def main(max_number_sticker, path_to_stick, path_to_cut_counter, path_to_save=No
         radius=DIAMETER_POINT / 2
     )
     # down right circle in dxf
-    msp.add_circle(
-        (A3[0] - 2 * mm - DIAMETER_POINT / 2, 2*Y_PAD + DIAMETER_POINT / 2),
-        DIAMETER_POINT / 2
+    new_msp.add_circle(
+        ((A3[0] - 2 * mm - DIAMETER_POINT / 2) / mm, (2 * Y_PAD + DIAMETER_POINT / 2) / mm),
+        DIAMETER_POINT / 2 / mm
     )
 
     if number_row > 1:
-        # up left circle
+        # up left circle in pdf
         draw_ref_circle(
             plot=c,
             x_cen=X_DISTANCE + DIAMETER_POINT / 2,
             y_cen=number_row * Y_PAD + HEIGHT_STICKER * (number_row - 1) + X_PAD,
             radius=DIAMETER_POINT / 2
         )
-        # up right circle
+        # up left circle in dxf
+        new_msp.add_circle(
+            ((X_DISTANCE + DIAMETER_POINT / 2) / mm, (number_row * Y_PAD + HEIGHT_STICKER * (number_row - 1) + X_PAD) / mm),
+            DIAMETER_POINT / 2 / mm
+        )
+
+        # up right circle in pdf
         draw_ref_circle(
             plot=c,
             x_cen=A3[0] - 2 * mm - DIAMETER_POINT / 2,
             y_cen=number_row * Y_PAD + HEIGHT_STICKER * (number_row - 1) + X_PAD,
             radius=DIAMETER_POINT / 2,
         )
+        # up right circle in dxf
+        new_msp.add_circle(
+            ((A3[0] - 2 * mm - DIAMETER_POINT / 2) / mm,
+             (number_row * Y_PAD + HEIGHT_STICKER * (number_row - 1) + X_PAD) / mm),
+            DIAMETER_POINT / 2 / mm
+        )
 
     # save pdf with stickers
     c.save()
     # save dxf file
-    doc.saveas("output.dxf")
+    new_doc.saveas("output.dxf")
 
 
 if __name__ == "__main__":
     main(
-        max_number_sticker=7, path_to_save="output.pdf",
+        max_number_sticker=24, path_to_save_pdf="output.pdf",
         path_to_stick="sticker.jpg",
-        path_to_cut_counter=...
+        path_to_counter="new.dxf"
     )
     # # read_xl(path_to_xl="input/template_sn.xlsx")
