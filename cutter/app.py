958f486555501620358b9cc5b0b7c813150585f5
Andrey Morozov <and.morozov.v@gmail.com>
2024-10-31 18:06:30 +0300

    added class Sticker and new logic work proj

--- cutter/app.py
+++ cutter/app.py
@@ -6,27 +6,22 @@ import svglib
 from svglib.svglib import svg2rlg
 
 from cutter import read_data
+from cutter.sticker import Sticker
 from cutter.draw import draw_sticker, draw_hline_ref_points
 from cutter.read_data import read_txt
 
 
 def main(
-        # data for sticker
-        path_to_sign, path_to_sticker,
-        width_sticker, height_sticker,
-        data, dx, dy,
+        stickers,
+        dx, dy,
         point_radius,
 ):
-    pdf = Canvas("new.pdf", pagesize=A3)
+    pdf = Canvas("output.pdf", pagesize=A3)
 
     x, y = point_radius * 2 + mm, 2 * mm
 
-    cnt_sticker = 1
+    ind_sticker = 0
     cnt_row = 1
-
-    # read the svg file for draw in a pdf
-    sticker_image = svg2rlg(path_to_sticker)
-    m_sticker_image = svg2rlg("template/modify_sticker.svg")
 
     # draw two ref point in below
     draw_hline_ref_points(
@@ -35,26 +30,26 @@ def main(
         radius=point_radius
     )
 
-    while cnt_sticker <= len(data):
+    while ind_sticker < len(stickers):
 
         # check filling on x
-        if x + width_sticker + 2 * point_radius > A3[0]:
+        if x + stickers[ind_sticker].width + 2 * point_radius > A3[0]:
             x = point_radius * 2 + mm
-            y += height_sticker + dy
+            y += stickers[ind_sticker].height + dy
 
             # check filling on y
-            if y + height_sticker > A3[1]:
+            if y + stickers[ind_sticker].height > A3[1]:
                 draw_hline_ref_points(
                     canvas=pdf,
                     x1_cen=point_radius, x2_cen=A3[0] - point_radius,
-                    y_cen=cnt_row * height_sticker + (cnt_row - 1) * dy - point_radius,
+                    y_cen=cnt_row * stickers[ind_sticker].height + (cnt_row - 1) * dy - point_radius,
                     radius=point_radius
                 )  # draw line ref point in upstairs
 
                 pdf.showPage()  # create new page
 
                 cnt_row = 1
-                x, y = point_radius * 2 + dx, 2 * mm
+                x, y = point_radius * 2 + mm, 2 * mm
                 # draw two ref point in below on new page
                 draw_hline_ref_points(
                     canvas=pdf,
@@ -64,19 +59,16 @@ def main(
                 continue
             cnt_row += 1
 
-        if cnt_sticker % 2 == 0:
-            draw_sticker(canvas=pdf, image=m_sticker_image, x=x+width_sticker, y=y-height_sticker)
-        else:
-            draw_sticker(canvas=pdf, image=sticker_image, x=x, y=y)
+        stickers[ind_sticker].draw_sticker_pdf(canvas=pdf, x=x, y=y)
 
-        x += width_sticker + dx
-        cnt_sticker += 1
+        x += stickers[ind_sticker].width + dx
+        ind_sticker += 1
 
     if cnt_row > 1:
         draw_hline_ref_points(
             canvas=pdf,
             x1_cen=point_radius, x2_cen=A3[0] - point_radius,
-            y_cen=cnt_row * height_sticker + (cnt_row - 1) * dy - point_radius,
+            y_cen=cnt_row * stickers[ind_sticker - 1].height + (cnt_row - 1) * dy - point_radius,
             radius=point_radius
         )  # draw line ref point in upstairs
 
@@ -84,19 +76,54 @@ def main(
 
 
 if __name__ == "__main__":
-    path_to_sticker = "template/sticker.svg"
+    paths_to_sticker = ["template/sticker_sn.svg", "template/reverse_sticker_sn.svg"]
     path_to_sn = "template/sn.svg"
     path_to_lot = "template/lot.svg"
 
     path_to_data = "input/sample.txt"
 
-    main(
-        path_to_sign=path_to_sn,
-        path_to_sticker=path_to_sticker,
+    # example for read data
+    # t = {"text": ..., "x": ..., "y": ..., "rotate": True, }
+    # text = [
+    #     {"text": "MCScap PROFESSIONAL, L,\n Mod: 15E-03M25\n mks.ru\n", "x": 132, "y": "fsdf", "rotate": 1},
+    #     {"text": "24123", "x":123, "y": 234}
+    # ]
 
-        data=read_txt(path_to_data),
-        dx=-7*mm, dy=2*mm, point_radius=3.5*mm,
-        width_sticker=46*mm, height_sticker=28*mm,
+    # read data for stickers
+    text = read_txt("input/sample.txt")
+
+    sticks = []
+    cnt = 1
+    for t in text:
+        if cnt % 2 == 0:
+
+            sticks.append(Sticker(
+                path_to_sticker="template/reverse_sticker_sn.svg", width=46 * mm, height=28 * mm,
+                text=[
+                        {"text": t[0], "x": 0, "y": 24*mm,},
+                        {"text": t[1], "x": 0, "y": 21*mm,},
+                        {"text": t[2], "x": 0, "y": 18*mm,},
+                        {"text": t[3], "x": 2.5*mm, "y": 11.5*mm,}
+                    ],
+                inverted=True)
+            )
+        else:
+            sticks.append(Sticker(
+                path_to_sticker="template/sticker_sn.svg", width=46 * mm, height=28 * mm,
+                text=[
+                        {"text": t[0], "x": 0, "y": 24*mm,},
+                        {"text": t[1], "x": 0, "y": 21*mm,},
+                        {"text": t[2], "x": 0, "y": 18*mm,},
+                        {"text": t[3], "x": 2.5*mm, "y": 11.5*mm,}
+                    ])
+            )
+        cnt += 1
+
+    main(
+        stickers=sticks,
+        dx=-7*mm,
+        dy=2*mm,
+        point_radius=3.5*mm,
     )
 
 
