19cdc7c06a6a4a77e59f47c3fde8c8931502c8fe
Andrey Morozov <and.morozov.v@gmail.com>
2024-11-07 14:58:19 +0300

    added create dxf file and draw contour

--- StickerCutter/main.py
+++ StickerCutter/main.py
@@ -1,6 +1,7 @@
 import datetime
 import os.path
 import logging
+import ezdxf
 
 from reportlab.lib.pagesizes import A3
 from reportlab.lib.units import mm
@@ -26,11 +27,13 @@ def main(
     pdf = Canvas(f"{dir_to_save}/output.pdf", pagesize=A3)
 
     # new dxf file
-    ...
+    dxf = ezdxf.new(stickers[0].doc_dxf.dxfversion)
+    msp = dxf.modelspace()
 
     # pad for draw annotate
-    x_pad, y_pad = 2*point_radius + mm, point_radius + mm
+    x_pad, y_pad = 2 * point_radius + mm, point_radius + mm
     x, y = x_pad, y_pad
+    x_, y_ = 0, 0  # coord for dxf file
 
     ind_sticker = 0
     cnt_row = 1
@@ -56,10 +59,13 @@ def main(
 
     while ind_sticker < len(stickers):
 
-        # check filling on x
+        # check filling on x and y
         if x + stickers[ind_sticker].width + 2 * point_radius > A3[0]:
             x = x_pad
             y += stickers[ind_sticker].height + dy
+
+            x_ = 0
+            y_ += (stickers[ind_sticker].height + dy) / mm
 
             # check filling on y
             if y + stickers[ind_sticker].height > A3[1]:
@@ -92,11 +98,13 @@ def main(
                     radius=point_radius
                 )
                 continue
-
             cnt_row += 1
 
+        # draw stickers
         stickers[ind_sticker].draw_sticker_pdf(canvas=pdf, x=x, y=y)
+        stickers[ind_sticker].draw_sticker_dxf(modelspace=msp, x=x_, y=y_)
 
+        x_ += (stickers[ind_sticker].width + dx) / mm
         x += stickers[ind_sticker].width + dx
         ind_sticker += 1
 
@@ -104,18 +112,20 @@ def main(
         draw_hline_ref_points(
             canvas=pdf,
             x1_cen=point_radius + mm, x2_cen=A3[0] - point_radius - mm,
-            y_cen=cnt_row * stickers[ind_sticker].height + (cnt_row - 1) * dy - point_radius,
+            y_cen=cnt_row * stickers[ind_sticker - 1].height + (cnt_row - 1) * dy - point_radius,
             radius=point_radius
         )  # draw line ref point in upstairs
 
     if ind_sticker == len(stickers):
         logging.info(f"The program worked well. Number of drawn stickers: {ind_sticker}."
                      f" Total number of stickers: {len(stickers)}")
-        logging.info(f"The result is saved to file: {dir_to_save}/output.pdf")
+        logging.info(f"The result is saved to file: {dir_to_save}/output.pdf.")
+        logging.info(f"Сutting file saved in {dir_to_save}/output.dxf.")
     else:
         logging.error(f"The number of stickers drawn does not correspond to the number of transferred ones.")
 
     pdf.save()
+    dxf.saveas(f"{dir_to_save}/output.dxf")
 
 
 if __name__ == "__main__":
@@ -139,6 +149,7 @@ if __name__ == "__main__":
         if cnt % 2 == 0:
             sticks.append(Sticker(
                 path_to_sticker="template/reverse_sticker_sn.svg",
+                path_to_dxf="template/sticker_reverse.dxf",
                 width=46 * mm, height=28 * mm,
                 text=[
                         {"text": t[0], "x": 0, "y": 24*mm}, {"text": t[1], "x": 0, "y": 21*mm},
@@ -149,6 +160,7 @@ if __name__ == "__main__":
         else:
             sticks.append(Sticker(
                 path_to_sticker="template/sticker_sn.svg",
+                path_to_dxf="template/sticker.dxf",
                 width=46 * mm, height=28 * mm,
                 text=[
                         {"text": t[0], "x": 0, "y": 24*mm}, {"text": t[1], "x": 0, "y": 21*mm},
--- StickerCutter/sticker.py
+++ StickerCutter/sticker.py
@@ -1,4 +1,7 @@
+import numpy as np
 import reportlab.pdfbase.pdfmetrics
+from ezdxf import select
+from ezdxf.layouts import Modelspace
 from reportlab.graphics import renderPDF
 from reportlab.lib.units import mm
 from reportlab.pdfbase import pdfmetrics
@@ -13,14 +16,43 @@ pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))
 
 class Sticker:
 
-    def __init__(self, width, height, path_to_sticker, text, inverted=False):
+    def __init__(
+            self,
+            # actual sticker sizes
+            width, height,
+
+            # paths to external files
+            path_to_sticker,
+            path_to_dxf,
+
+            # information on the sticker
+            text,
+            inverted=False
+    ):
+        # read file with pic sticker
         self.image = svg2rlg(path_to_sticker)
+
+        # work with dxf file
+        self.doc_dxf = ezdxf.readfile(path_to_dxf)
+        self.msp_dxf = self.doc_dxf.modelspace()
 
         self.width = width
         self.height = height
         self.text = text
 
         self.inverted = inverted
+
+        # get entities and left-down point
+        window = select.Window(
+            (float("-inf"), float("-inf")),
+            (float("inf"), float("inf"))
+        )
+        self.entities = select.bbox_outside(window, self.msp_dxf.entity_space.entities)     # elements in dxf file
+        self.min_pt = None      # left-down point (using for draw dxf cutting file)
+        for entity in self.entities:
+            pt = np.min(np.array(entity.control_points), axis=0)
+            if self.min_pt is None or np.all(pt < self.min_pt):
+                self.min_pt = pt    # anchor point
 
     # function for draw in pdf
 
@@ -30,11 +62,9 @@ class Sticker:
         # draw sticker
         renderPDF.draw(self.image, canvas, x, y)
         if self.text is not None:
-
             if self.inverted:
                 canvas.saveState()
 
-                # ToDo: figure out - why are there offsets?
                 canvas.translate(x + self.width - 0.5 * mm, y + self.height + 0.25 * mm)
                 canvas.rotate(180)
 
@@ -53,7 +83,12 @@ class Sticker:
         canvas.drawString(x + int((self.width - text_width)) / 2, y, text)
 
     # function for draw in dxf
-    ...
+    def draw_sticker_dxf(self, modelspace: Modelspace, x: float, y: float,):
+        # draw the cutting outline in dxf
+        for entity in self.entities:
+            cp_entity = entity.copy()
+            cp_entity.translate(-self.min_pt[0] + x, -self.min_pt[1] + y, 0)
+            modelspace.add_foreign_entity(cp_entity)
 
 
 class RefPoint:
